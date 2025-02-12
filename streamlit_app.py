98a99b53d3a08f4a678e35385b2504d4436bd953
Jeff Nuyts <NUYJE@sintcordula.be>
2025-02-12 11:02:10 +0100

    sqdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,22 @@ import streamlit as st
 
 animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
 
-# Initialiseer sessievariabele voor input
+# Functie om het invoerveld te resetten
+def clear_input():
+    st.session_state.animal_input = ""  # Zet de waarde leeg
+    st.rerun()  # Herstart de app zodat de wijziging zichtbaar wordt
+
+# Initialiseer sessievariabele als die nog niet bestaat
 if "animal_input" not in st.session_state:
     st.session_state.animal_input = ""
 
-# Tekstveld gekoppeld aan sessievariabele
+# Tekstveld ZONDER `value=`
 animal = st.text_input('Type an animal', key="animal_input")
 
 if st.button('Check availability'):
     if st.session_state.animal_input.lower() in animal_shelter:
-        st.success('We have that animal!')  
+        st.success('We have that animal!')
     else:
         st.error('We don\'t have that animal.')
 
-    # **Leegmaken van de sessievariabele**
-    st.session_state.animal_input = ""
-    
-    # **Forceer update van het tekstveld**
-    st.rerun()
+    clear_input()  # Roep de functie aan om de invoer te wissen
