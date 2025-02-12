1f7157869d1f59fde579378538df390912e6f872
Jeff Nuyts <NUYJE@sintcordula.be>
2025-02-12 10:33:51 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,22 @@
 import streamlit as st
-if "animal" not in st.session_state:
-    st.session_state.animal_input = ""
 
 animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
 
-animal = st.text_input('Type an animal', value=st.session_state.animal_input, key="animal")
+# Initialiseer sessievariabele voor input
+if "animal_input" not in st.session_state:
+    st.session_state.animal_input = ""
+
+# Tekstveld gekoppeld aan sessievariabele
+animal = st.text_input('Type an animal', key="animal_input")
 
 if st.button('Check availability'):
-    have_it = animal.lower() in animal_shelter
-    if have_it:
-        'We have that animal!' 
-        animal = "" 
+    if st.session_state.animal_input.lower() in animal_shelter:
+        st.success('We have that animal!')  
     else:
-        'We don\'t have that animal.'
+        st.error('We don\'t have that animal.')
 
-st.session_state.animal = ""  
-st.rerun()  # Herlaad de app om de v
+    # **Leegmaken van de sessievariabele**
+    st.session_state.animal_input = ""
+    
+    # **Forceer update van het tekstveld**
+    st.rerun()
