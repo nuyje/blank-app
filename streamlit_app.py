532622f0698a453d7b72a3e9e94067c6d31005ab
Jeff Nuyts <NUYJE@sintcordula.be>
2025-02-13 12:56:45 +0100

    qsdffdsgq

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,20 +5,19 @@ animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
 if "animal_input" not in st.session_state:
     st.session_state.animal_input = ""
 
-animal = st.session_state.animal_input
-
 def clear_input():
     st.session_state["animal_input"] = ""
 
 def check_availability(): 
-    if animal.lower() in animal_shelter:
+    if st.session_state.animal_input.lower() in animal_shelter:
         st.success('We have that animal!')
     else:
         st.error('We don\'t have that animal.')
 
+
+
+st.session_state.animal_input = st.text_input('Type an animal', key="animal_input")
+
+check_availability()
 clear_input()
-
-animal = st.text_input('Type an animal', key="animal_input", on_change=check_availability)
-
-
 
