f1b4281f9dc1ed996c5b8f3f3837e39aab4f8269
Jeff Nuyts <NUYJE@sintcordula.be>
2025-02-13 12:23:59 +0100

    sqfd

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
 
-
-# Functie om het invoerveld leeg te maken
-def clear_input():
-    st.session_state["animal_input"] = ""  # Zet de sessievariabele leeg
-
-# Tekstveld wordt volledig beheerd via `st.session_state`
 animal = st.text_input('Type an animal', key="animal_input")
+
+def clear_input():
+    st.session_state["animal_input"] = ""
+
+st.button("clear text input", on_click=clear_text)
+st.write(input)
 
 if st.button('Check availability'):
     if st.session_state.animal_input.lower() in animal_shelter:
