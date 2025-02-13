82623b88a76eec6cffd83a78b705a2ebe6646592
Jeff Nuyts <NUYJE@sintcordula.be>
2025-02-13 12:26:40 +0100

    qsdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,15 @@ import streamlit as st
 
 animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
 
-animal = st.text_input('Type an animal', key="animal_input")
+
 
 def clear_input():
     st.session_state["animal_input"] = ""
 
 st.button("clear text input", on_click=clear_input)
 st.write(input)
+
+animal = st.text_input('Type an animal', key="animal_input")
 
 if st.button('Check availability'):
     if st.session_state.animal_input.lower() in animal_shelter:
