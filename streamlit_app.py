2803fbe33907149b709999d57ed3651d9260f9b8
Jeff Nuyts <NUYJE@sintcordula.be>
2025-02-13 12:43:42 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,16 @@ import streamlit as st
 
 animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
 
+if "animal_input" not in st.session_state:
+    st.session_state.animal_input = ""
+
 def clear_input():
     st.session_state["animal_input"] = ""
 
 clear_input()
 
-animal = st.text_input('Type an animal', key="animal_input")
+st.text_input('Type an animal', key="animal_input")
+animal = st.session_state.animal_input
 
 if st.button('Check availability'):
     if animal.lower() in animal_shelter:
