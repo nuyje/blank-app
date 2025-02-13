2dc8304eaa5320868deb56b13e61d37b7a474714
Jeff Nuyts <NUYJE@sintcordula.be>
2025-02-13 12:38:39 +0100

    sqdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,5 @@ if st.button('Check availability'):
     if st.session_state.animal_input.lower() in animal_shelter:
         st.success('We have that animal!')
     else:
-        st.error(f'We don\'t have {animal} that animal.')
+        st.error('We don\'t have that animal.')
 
